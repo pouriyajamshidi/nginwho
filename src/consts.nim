0489a5311b58bad46875ec305c4b2edbdb68469e
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2026-09-13 14:33:58 +0200

    read only the new part of the log in 1 MB chunks to use less memory

--- src/consts.nim
+++ src/consts.nim
@@ -13,7 +13,7 @@ const
 
   NGINWHO_DB_FILE*: string = "/var/log/nginwho.db"
   MAX_INSERT_ATTEMPTS*: int = 3
-  READ_CHUNK_BYTES*: int = 16 * 1024 * 1024
+  READ_CHUNK_BYTES*: int = 1024 * 1024
 
   TEN_SECONDS*: int = int(initDuration(seconds = 10).inMilliseconds)
   SIX_HOURS*: int = int(initDuration(hours = 6).inMilliseconds)
